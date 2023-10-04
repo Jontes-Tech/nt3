bd75a5d0277035d38a7907e070b1ed188640d386
Jonatan Holmgren <jonatan@jontes.page>
2023-10-04 16:54:42 +0200

    Searching

--- src/SearchPage.svelte
+++ src/SearchPage.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import Special from "./Special.svelte";
+  import { writable } from "svelte/store";
 
   interface Result {
     title: string;
@@ -15,7 +17,7 @@
   let loaded = false;
   let special: Result | null = null;
   if (!query) {
-    window.location.href = "/";
+    console.log(query);
   } else {
     if (token) {
       history.replaceState({}, "", location.pathname + "?query=" + query);
@@ -31,11 +33,15 @@
       if (res.status === 403) {
         window.location.href = "/intermediate?query=" + query;
       } else if (res.status === 429) {
-        console.log(res.headers.get("x-ratelimit-reset"))
+        console.log(res.headers.get("x-ratelimit-reset"));
         alert(
           "You're searching too fast! You can't search for " +
             res.headers.get("x-ratelimit-reset") +
             " seconds."
+        );
+      } else if (!res.ok) {
+        alert(
+          "I ain't got no clue what happened, but it for sure wasn't good."
         );
       }
       const between = await res.json();
@@ -62,6 +68,37 @@
     search();
   }
   const specialDomains = ["www.youtube.com", "jontes.page"];
+
+  onMount(() => {
+    const searching = document.getElementById("searching");
+    let funnyTexts = [
+      "Searching",
+      "Still searching",
+      "Something's most definitely wrong with your internet",
+      "Nah, we screwed fam",
+      "We're probably not gonna find anything",
+      "But if we do, it's gonna be a miracle",
+      "Eh, you might as well give up",
+    ];
+    const interval = setInterval(() => {
+      if (loaded) {
+        clearInterval(interval);
+        if (searching?.innerText !== "Searching") {
+          new Audio(
+            "https://i1.theportalwiki.net/img/5/56/GLaDOS_05_part1_success-1.wav"
+          ).play();
+          // Haha, sneaky Portal reference
+        }
+        return;
+      }
+      if (searching) {
+        const index = funnyTexts.indexOf(searching.innerText);
+        if (index !== funnyTexts.length - 1) {
+          searching.innerText = funnyTexts[index + 1];
+        }
+      }
+    }, 2500);
+  });
 </script>
 
 {#if loaded}
@@ -91,12 +128,16 @@
         </li>
       </a>
     {/each}
-    Results by Google | Please don't scrape this page
+    Results by Google | I am paying, don't be evil
   </ol>
 {:else}
-  <div class="flex justify-center items-center h-screen">
-    <div
-      class="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"
-    />
-  </div>
+  <img
+    src="/web.svg"
+    alt="The Web Times"
+    class="mb-2 mx-auto"
+    draggable="false"
+  />
+  <p class="text-2xl mt-12">
+    <span id="searching">Searching</span>
+  </p>
 {/if}
